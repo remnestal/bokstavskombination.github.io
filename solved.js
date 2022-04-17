464fa291cfa60495bf454e20a2dd44e50374cf2a
bokstavskombination <103757315+bokstavskombination@users.noreply.github.com>
2022-04-17 19:21:28 +0200

    Create solved.js

---
+++ solved.js
@@ -0,0 +1,9 @@
+function expandSolvedExpandable() {
+  document.getElementById("solved-expandable").classList.toggle("fade-in");
+  document.getElementById("solved-expandable").classList.toggle("fade-out");
+}
+
+function collapseSolvedExpandable() {
+  document.getElementById("solved-expandable").classList.toggle("fade-out");
+  document.getElementById("solved-expandable").classList.toggle("fade-in");
+}
