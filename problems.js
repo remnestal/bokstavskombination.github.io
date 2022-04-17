26a3a6794527bfda9a6bc4d693b63de80f56ce59
bokstavskombination <103757315+bokstavskombination@users.noreply.github.com>
2022-04-17 19:21:45 +0200

    Create problems.js

---
+++ problems.js
@@ -0,0 +1,243 @@
+window.problems = [
+	{
+		"date": "2022-04-17",
+		"characters": {
+			"fixed": "d",
+			"optional": [
+				"t",
+				"v",
+				"k",
+				"i",
+				"l",
+				"a"
+			]
+		},
+		"total_score": 259,
+		"words": {
+			"additiv": {
+				"value": 7,
+				"panagram": false
+			},
+			"adla": {
+				"value": 1,
+				"panagram": false
+			},
+			"alltid": {
+				"value": 6,
+				"panagram": false
+			},
+			"avlida": {
+				"value": 6,
+				"panagram": false
+			},
+			"dadda": {
+				"value": 5,
+				"panagram": false
+			},
+			"dala": {
+				"value": 1,
+				"panagram": false
+			},
+			"dalt": {
+				"value": 1,
+				"panagram": false
+			},
+			"dalta": {
+				"value": 5,
+				"panagram": false
+			},
+			"data": {
+				"value": 1,
+				"panagram": false
+			},
+			"dativ": {
+				"value": 5,
+				"panagram": false
+			},
+			"datt": {
+				"value": 1,
+				"panagram": false
+			},
+			"didaktik": {
+				"value": 8,
+				"panagram": false
+			},
+			"dika": {
+				"value": 1,
+				"panagram": false
+			},
+			"dikalv": {
+				"value": 6,
+				"panagram": false
+			},
+			"dikt": {
+				"value": 1,
+				"panagram": false
+			},
+			"dikta": {
+				"value": 5,
+				"panagram": false
+			},
+			"diktat": {
+				"value": 6,
+				"panagram": false
+			},
+			"dill": {
+				"value": 1,
+				"panagram": false
+			},
+			"dilla": {
+				"value": 5,
+				"panagram": false
+			},
+			"ditt": {
+				"value": 1,
+				"panagram": false
+			},
+			"diva": {
+				"value": 1,
+				"panagram": false
+			},
+			"dvala": {
+				"value": 5,
+				"panagram": false
+			},
+			"idka": {
+				"value": 1,
+				"panagram": false
+			},
+			"kadi": {
+				"value": 1,
+				"panagram": false
+			},
+			"kalkad": {
+				"value": 6,
+				"panagram": false
+			},
+			"kavalkad": {
+				"value": 8,
+				"panagram": false
+			},
+			"kladd": {
+				"value": 5,
+				"panagram": false
+			},
+			"kladda": {
+				"value": 6,
+				"panagram": false
+			},
+			"kladdkaka": {
+				"value": 9,
+				"panagram": false
+			},
+			"kvad": {
+				"value": 1,
+				"panagram": false
+			},
+			"kvadda": {
+				"value": 6,
+				"panagram": false
+			},
+			"kvaltid": {
+				"value": 14,
+				"panagram": true
+			},
+			"kvida": {
+				"value": 5,
+				"panagram": false
+			},
+			"kvidd": {
+				"value": 5,
+				"panagram": false
+			},
+			"lada": {
+				"value": 1,
+				"panagram": false
+			},
+			"ladda": {
+				"value": 5,
+				"panagram": false
+			},
+			"laddad": {
+				"value": 6,
+				"panagram": false
+			},
+			"ladtak": {
+				"value": 6,
+				"panagram": false
+			},
+			"lida": {
+				"value": 1,
+				"panagram": false
+			},
+			"likvid": {
+				"value": 6,
+				"panagram": false
+			},
+			"likvida": {
+				"value": 7,
+				"panagram": false
+			},
+			"livad": {
+				"value": 5,
+				"panagram": false
+			},
+			"taltid": {
+				"value": 6,
+				"panagram": false
+			},
+			"tilldiktad": {
+				"value": 10,
+				"panagram": false
+			},
+			"vada": {
+				"value": 1,
+				"panagram": false
+			},
+			"vadd": {
+				"value": 1,
+				"panagram": false
+			},
+			"valid": {
+				"value": 5,
+				"panagram": false
+			},
+			"valtid": {
+				"value": 6,
+				"panagram": false
+			},
+			"vida": {
+				"value": 1,
+				"panagram": false
+			},
+			"vidd": {
+				"value": 1,
+				"panagram": false
+			},
+			"vidi": {
+				"value": 1,
+				"panagram": false
+			},
+			"vidta": {
+				"value": 5,
+				"panagram": false
+			},
+			"vidtala": {
+				"value": 7,
+				"panagram": false
+			},
+			"vild": {
+				"value": 1,
+				"panagram": false
+			},
+			"vildkatt": {
+				"value": 15,
+				"panagram": true
+			},
+			"vildkatta": {
+				"value": 16,
+				"panagram": true
+			}
+		}
+	}
+]
